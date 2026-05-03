2f1ee70870ae6b42e08a014e3f68be801be9f595
Ashaaf92 <ashaaf92@gmail.com>
2026-05-03 11:58:15 +0530

    added negative counter too

--- react-refresher/src/App.jsx
+++ react-refresher/src/App.jsx
@@ -9,6 +9,8 @@ const [count, setCount] = useState(0)
         <div>
            <h1>{count}</h1>
            <button onClick = {()=>setCount(count+1)}>➕</button>
+            <button onClick = {()=>setCount(count-1)}>➖</button>
+
         </div>
     )
 }
